aa537565b175a2c27b8c5828716523630a871c28
Yevgniya Skubiy <yevgeniyaskubiy@gmail.com>
2025-05-11 19:14:56 +0300

    Додати сторінку Клієнти

--- server.js
+++ server.js
@@ -14,29 +14,39 @@ mongoose
 
 app.use(express.static('public'));
 
-const transactionSchema = new mongoose.Schema({
-  accountId: mongoose.Types.ObjectId,
-  type: String,
-  amount: Number,
-  date: Date,
-  description: String,
+const clientSchema  = new mongoose.Schema({
+  fullName: String,
+  passport: String,
+  phone: String,
+  email: String,
 });
 
-const Transaction = mongoose.model("Transaction", transactionSchema, "transactions");
+const Client = mongoose.model("Client", clientSchema, "clients");
 
 app.use(cors());
 app.use(express.json());
 
-// 📍 API: Отримати всі транзакції
-app.get("/transactions", async (req, res) => {
+// 📍 API: Отримати всі клієнти
+app.get("/clients", async (req, res) => {
   try {
-    const transactions = await Transaction.find().sort({ date: -1 });
-    res.json(transactions);
+    const clients = await Client.find();
+    res.json(clients);
   } catch (err) {
-    console.error("❌ Помилка отримання транзакцій:", err);
+    console.error("❌ Помилка отримання клієнтів:", err);
     res.status(500).json({ error: "Помилка сервера" });
   }
 });
+// const Transaction = mongoose.model("Transaction", transactionSchema, "transactions");
+// // 📍 API: Отримати всі транзакції
+// app.get("/transactions", async (req, res) => {
+//   try {
+//     const transactions = await Transaction.find().sort({ date: -1 });
+//     res.json(transactions);
+//   } catch (err) {
+//     console.error("❌ Помилка отримання транзакцій:", err);
+//     res.status(500).json({ error: "Помилка сервера" });
+//   }
+// });
 
 app.listen(PORT, () => {
   console.log(`🚀 Сервер працює на http://localhost:${PORT}`);
